9610b192c2eedfe42b6e10e9be2db132e0d12993
lucalee <30341309+lucalee-1@users.noreply.github.com>
2024-08-24 17:36:41 +0900

    feat: install and configure tanstack router

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-import { vitePlugin as remix } from "@remix-run/dev";
-import { installGlobals } from "@remix-run/node";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-
-installGlobals();
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import viteReact from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [remix(), tsconfigPaths()],
+  plugins: [TanStackRouterVite(), viteReact(), tsconfigPaths()],
 });
