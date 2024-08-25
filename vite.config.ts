cb0bf0f9e18cec0cd9baf5cd9fb7dc0708eddad7
lucalee <30341309+lucalee-1@users.noreply.github.com>
2024-08-25 15:12:06 +0900

    feat: install and configure powersync

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,27 @@
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import viteReact from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, Plugin } from 'vite';
+import topLevelAwait from 'vite-plugin-top-level-await';
+import wasm from 'vite-plugin-wasm';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [TanStackRouterVite(), viteReact(), tsconfigPaths()],
+  envDir: '..',
+  optimizeDeps: {
+    // Don't optimize these packages as they contain web workers and WASM files.
+    // https://github.com/vitejs/vite/issues/11672#issuecomment-1415820673
+    exclude: ['@journeyapps/wa-sqlite', '@powersync/web'],
+    include: [],
+  },
+  plugins: [
+    TanStackRouterVite(),
+    viteReact(),
+    tsconfigPaths(),
+    wasm(),
+    topLevelAwait(),
+  ],
+  worker: {
+    format: 'es',
+    plugins: () => [wasm(), topLevelAwait()] as Plugin[],
+  },
 });
