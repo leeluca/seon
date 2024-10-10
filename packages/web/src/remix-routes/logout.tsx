da59a3b5556f5d68bfa9cd8e00b6f018a91f1930
leeluca <30341309+leeluca@users.noreply.github.com>
2024-10-10 20:59:43 +0900

    refactor: remove unused directories and files

--- packages/web/src/remix-routes/logout.tsx
+++
@@ -1,6 +0,0 @@
-import type { LoaderFunctionArgs } from '@remix-run/node';
-import { authenticator } from '~/.server/services/auth';
-
-export async function loader({ request }: LoaderFunctionArgs) {
-  await authenticator.logout(request, { redirectTo: '/login' });
-}
