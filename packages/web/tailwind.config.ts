632699a8f9399333669331bc2978e47c7daaec3b
leeluca <30341309+leeluca@users.noreply.github.com>
2024-10-27 15:33:54 +0900

    feat: add placeholder for when there are no goals

--- packages/web/tailwind.config.ts
+++ packages/web/tailwind.config.ts
@@ -68,10 +68,15 @@ export default{
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        fadeIn: {
+          from: { opacity: '0' },
+          to: { opacity: '1' },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "delayed-fade-in": "fadeIn 0.25s ease-in-out 0.4s forwards"
       },
     },
   },
