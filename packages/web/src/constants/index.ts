6545f0499600deb8036fea7611cee0f3ce9d2606
leeluca <30341309+leeluca@users.noreply.github.com>
2024-10-27 15:33:04 +0900

    feat: add CDN url env config

--- packages/web/src/constants/index.ts
+++ packages/web/src/constants/index.ts
@@ -1,4 +1,5 @@
 export const API_URL = import.meta.env.VITE_API_URL;
+export const CDN_URL = import.meta.env.VITE_CDN_URL;
 
 export const MAX_INPUT_NUMBER = 100000000; // 100 million
 
--- packages/web/src/types/global/vite-env.d.ts
+++ packages/web/src/types/global/vite-env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
   readonly VITE_SUPABASE_URL: string;
   readonly VITE_SUPABASE_ANON_KEY: string;
   readonly VITE_API_URL: string;
+  readonly VITE_CDN_URL: string;
   readonly VITE_POWERSYNC_URL?: string;
   readonly VITE_POWERSYNC_DEV_TOKEN?: string;
 }
