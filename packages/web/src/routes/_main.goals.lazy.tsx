7dfd0cb8b86616783ff1eeeb26d9aa949c32b431
leeluca <30341309+leeluca@users.noreply.github.com>
2024-12-17 23:30:05 +0900

    feat: upgrade lingui library to v5

--- packages/web/src/routes/_main.goals.lazy.tsx
+++ packages/web/src/routes/_main.goals.lazy.tsx
@@ -1,5 +1,5 @@
 import { lazy, Suspense } from 'react';
-import { Trans } from '@lingui/macro';
+import { Trans } from '@lingui/react/macro';
 import { useQuery, useStatus } from '@powersync/react';
 import {
   createLazyFileRoute,
