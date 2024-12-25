e9d88cb289b0b11ef63395b86ba712298299cb80
leeluca <30341309+leeluca@users.noreply.github.com>
2024-12-25 17:49:42 +0900

    feat: show goal edit form opened as default, style adjustments

--- packages/web/src/components/GoalDetailPanel.tsx
+++ packages/web/src/components/GoalDetailPanel.tsx
@@ -69,8 +69,8 @@ export function GoalDetailPanel({
           <SheetTitle className="text-2xl">{title}</SheetTitle>
           <SheetDescription>{description}</SheetDescription>
         </SheetHeader>
-        <div className="flex h-full flex-col gap-1">
-          <div className="my-5 min-h-[20%]">
+        <article className="flex h-full flex-col gap-1">
+          <section className="my-4 min-h-[20%]">
             <GoalLineGraph
               key={`${id}-graph`}
               goalId={id}
@@ -79,9 +79,9 @@ export function GoalDetailPanel({
               startDate={startDate}
               initialValue={initialValue}
             />
-          </div>
+          </section>
           <GoalEditForm goal={selectedGoal} />
-        </div>
+        </article>
         <SheetFooter>
           <SheetClose asChild></SheetClose>
         </SheetFooter>
@@ -148,7 +148,7 @@ function GoalEditForm({ goal }: { goal: Database['goal'] }) {
     updatedAt,
   } = goal;
 
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(true);
 
   const form = useForm<NewGoal>({
     defaultValues: {
@@ -188,7 +188,7 @@ function GoalEditForm({ goal }: { goal: Database['goal'] }) {
   return (
     <section className="my-4 overflow-y-auto">
       <Collapsible open={isOpen} onOpenChange={setIsOpen}>
-        <div className="mb-1 flex h-[3.75rem] items-end justify-between">
+        <div className="flex h-[3.75rem] items-start justify-between">
           <div className="flex items-center">
             <CollapsibleTrigger asChild className="mr-4">
               <Button id="edit-goal-toggle" size="icon-sm" variant="secondary">
@@ -277,7 +277,7 @@ function GoalEditForm({ goal }: { goal: Database['goal'] }) {
         <CollapsibleContent>
           <GoalForm
             form={form}
-            formItemClassName="grid-cols-1 items-start gap-y-3 px-1"
+            formItemClassName="grid-cols-1 items-start gap-y-2 px-1"
           />
         </CollapsibleContent>
       </Collapsible>
