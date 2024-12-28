674a587875307c233af48bb9461e6880384f9b7a
leeluca <30341309+leeluca@users.noreply.github.com>
2024-12-28 14:29:24 +0900

    feat: dynamically import date-fns locale

--- packages/web/src/locales/i18n.ts
+++ packages/web/src/locales/i18n.ts
@@ -3,7 +3,6 @@ import type { Messages } from '@lingui/core';
 import { i18n } from '@lingui/core';
 import { fromNavigator, multipleDetect } from '@lingui/detect-locale';
 import { setDefaultOptions } from 'date-fns';
-import { enUS, ko } from 'date-fns/locale';
 
 export const locales = {
   en: 'English',
@@ -15,18 +14,20 @@ export const defaultLocale = multipleDetect(fromNavigator(), DEFAULT_FALLBACK)
   .map((locale) => locale.split('-')[0])
   .find((locale) => locale in locales) as keyof typeof locales;
 
-// FIXME: dynamically import only the necessary locales
-const dateFnsLocaleMap = {
-  en: enUS,
-  ko,
-};
-setDefaultOptions({ locale: dateFnsLocaleMap[defaultLocale] });
-
 export async function dynamicallyImportLocale(locale: keyof typeof locales) {
-  const { messages } = (await import(`../locales/${locale}/messages.ts`)) as {
-    messages: Messages;
+  const dateFnslocaleMap = {
+    en: async () => (await import('date-fns/locale/en-US')).enUS,
+    ko: async () => (await import('date-fns/locale/ko')).ko,
   };
 
-  i18n.load(locale, messages);
+  const [importedMessages, dfnsLocale] = await Promise.all([
+    import(`../locales/${locale}/messages.ts`) as Promise<{
+      messages: Messages;
+    }>,
+    dateFnslocaleMap[locale](),
+  ]);
+
+  i18n.load(locale, importedMessages.messages);
   i18n.activate(locale);
+  setDefaultOptions({ locale: dfnsLocale });
 }
