301466c8e0f215c2e6ce19ec19d85270ba7ca2f9
leeluca <30341309+leeluca@users.noreply.github.com>
2025-01-05 20:14:53 +0900

    test

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,32 +1,9 @@
-import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
-import { cors } from 'hono/cors';
-
-import { ORIGIN_URLS } from './constants/config.js';
-import auth from './routes/auth.js';
 
 const app = new Hono();
-app.get('/', (c) => c.text('Hello world!'))
 
-app.use(
-  '/api/*',
-  cors({
-    origin: ORIGIN_URLS,
-    allowHeaders: ['Origin', 'X-Requested-With', 'User-Agent', 'Content-Type'],
-    allowMethods: ['OPTIONS', 'HEAD', 'GET', 'POST'],
-    maxAge: 7200,
-    credentials: true,
-  }),
-);
-
-app.route('/api/auth', auth);
-
-const port = 3000;
-console.log(`Server is running on port ${port}`);
-
-// serve({
-//   fetch: app.fetch,
-//   port,
-// });
+app.get('/', (c) => {
+  return c.text('Hello Hono!');
+});
 
 export default app;
