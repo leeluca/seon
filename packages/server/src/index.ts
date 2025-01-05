e7d62655bfa3a917ba8eebc50db81973c44600ce
leeluca <30341309+leeluca@users.noreply.github.com>
2025-01-05 21:16:46 +0900

    feat: add docker file

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,9 +1,32 @@
+import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
+import { cors } from 'hono/cors';
+
+import { ORIGIN_URLS } from './constants/config.js';
+import auth from './routes/auth.js';
 
 const app = new Hono();
+app.get('/', (c) => c.text('Hello World!'))
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!');
+app.use(
+  '/api/*',
+  cors({
+    origin: ORIGIN_URLS,
+    allowHeaders: ['Origin', 'X-Requested-With', 'User-Agent', 'Content-Type'],
+    allowMethods: ['OPTIONS', 'HEAD', 'GET', 'POST'],
+    maxAge: 7200,
+    credentials: true,
+  }),
+);
+
+app.route('/api/auth', auth);
+
+const port = 3000;
+console.log(`Server is running on port ${port}`);
+
+serve({
+  fetch: app.fetch,
+  port,
 });
 
 export default app;
