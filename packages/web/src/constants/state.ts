14b6f42ceef97d05135817d3709dcb58b62b85df
leeluca <30341309+leeluca@users.noreply.github.com>
2025-01-14 19:51:21 +0900

    refactor: move AUTH_CONTEXT_INITIAL_STATE value to separate constants file

---
+++ packages/web/src/constants/state.ts
@@ -0,0 +1,9 @@
+export const AUTH_CONTEXT_INITIAL_STATE = {
+    isSignedIn: false,
+    isSignInVerified: false,
+    expiresAt: 0,
+    isLoading: false,
+    isError: false,
+    error: undefined,
+  };
+  
