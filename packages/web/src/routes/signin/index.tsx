cfcb7f7e624635edc3073ecfa61e66725f943796
leeluca <30341309+leeluca@users.noreply.github.com>
2025-02-06 12:43:30 +0900

    fix: authStatus context value on sign in and sign out routes

--- packages/web/src/routes/signin/index.tsx
+++ packages/web/src/routes/signin/index.tsx
@@ -2,7 +2,7 @@ import { createFileRoute, redirect } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/signin/')({
   beforeLoad({ context }) {
-    if (context.authStatus.isSignedIn) {
+    if (context.authStatus.result) {
       return redirect({ to: '/goals' })
     }
   },
