8c8be094e8b3d22b7f21f9550b2f518bbf236b1e
leeluca <30341309+leeluca@users.noreply.github.com>
2025-04-29 15:16:18 +0900

    ci: configure CI env

--- packages/web/playwright.config.ts
+++ packages/web/playwright.config.ts
@@ -3,6 +3,11 @@ import { defineConfig, devices } from '@playwright/test';
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
+
+const BASE_URL = process.env.CI
+  ? 'http://localhost:4173'
+  : 'http://localhost:5173';
+
 export default defineConfig({
   testDir: './tests/e2e',
   /* Run tests in files in parallel */
@@ -12,26 +17,36 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:5173',
+    baseURL: BASE_URL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+  },
+
+  expect: {
+    timeout: 2000,
   },
 
   /* Configure projects for major browsers */
   projects: [
     // NOTE: Safari does not work in parallel as it needs a persistent context to use OPFS
-    {
-      name: 'Mobile Safari',
-      use: { ...devices['iPhone 12'] },
-      workers: 1,
-    },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    //   workers: 1,
+    // },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    //   workers: 1,
+    // },
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
@@ -41,15 +56,11 @@ export default defineConfig({
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
+
     // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    //   workers: 1,
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['Pixel 5'] },
     // },
-    {
-      name: 'Mobile Chrome',
-      use: { ...devices['Pixel 5'] },
-    },
 
     /* Test against branded browsers. */
     // {
@@ -64,8 +75,8 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'VITE_IS_DEMO=true pnpm dev',
-    url: 'http://localhost:5173',
+    command: process.env.CI ? 'pnpm serve' : 'VITE_IS_DEMO=true pnpm dev',
+    url: BASE_URL,
     reuseExistingServer: !process.env.CI,
     stdout: 'pipe',
     stderr: 'pipe',
