ff4ccfb8855bdc8950ed224d8b1c1b6c9c79da1e
leeluca <30341309+leeluca@users.noreply.github.com>
2025-05-06 21:37:28 +0900

    feat: add NotFound component

---
+++ packages/web/src/components/NotFound.tsx
@@ -0,0 +1,41 @@
+import { Trans } from '@lingui/react/macro';
+import { Link } from '@tanstack/react-router';
+import { HomeIcon } from 'lucide-react';
+
+import { cn } from '~/utils';
+import { Button } from './ui/button';
+
+interface NotFoundProps {
+  className?: string;
+}
+
+function NotFound({ className }: NotFoundProps) {
+  return (
+    <div
+      className={cn(
+        'bg-background flex min-h-[50vh] flex-col items-center justify-center p-4 text-center',
+        className,
+      )}
+    >
+      <h1 className="mb-4 text-2xl font-bold">
+        <Trans>Page Not Found</Trans>
+      </h1>
+      <p className="text-muted-foreground mb-6 max-w-md">
+        <Trans>
+          The page you're looking for doesn't exist or has been moved.
+        </Trans>
+      </p>
+
+      <div className="flex w-64 flex-col gap-4 *:w-full">
+        <Button asChild>
+          <Link to="/" replace>
+            <HomeIcon className="mr-2 h-4 w-4" />
+            <Trans>Return to Home</Trans>
+          </Link>
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default NotFound;
