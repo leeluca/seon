ed7bfd8dd395a4b027a322a0ba6ad148a28a6f94
leeluca <30341309+leeluca@users.noreply.github.com>
2025-12-04 22:44:36 +0900

    test: fix unit tests

--- packages/web/tests/test-utils.tsx
+++ packages/web/tests/test-utils.tsx
@@ -1,22 +1,25 @@
 import type React from 'react';
-import { i18n } from '@lingui/core';
+import { i18n, type Messages } from '@lingui/core';
 import { I18nProvider } from '@lingui/react';
 import { render, type RenderOptions } from '@testing-library/react';
 
-import { messages as enMessages } from '~/locales/en/messages';
-import { messages as koMessages } from '~/locales/ko/messages';
+import { messages as enMessages } from '~/locales/en/messages.po';
+import { messages as koMessages } from '~/locales/ko/messages.po';
 
 const ContextProviders = ({ children }: { children: React.ReactNode }) => {
   i18n.load({
-    en: enMessages,
-    ko: koMessages,
+    en: enMessages as Messages,
+    ko: koMessages as Messages,
   });
   i18n.activate('en');
 
   return <I18nProvider i18n={i18n}>{children}</I18nProvider>;
 };
 
-const customRender = (ui: React.ReactNode, options?: RenderOptions) =>
+const customRender = (
+  ui: React.ReactNode,
+  options?: RenderOptions,
+): ReturnType<typeof render> =>
   render(ui, { wrapper: ContextProviders, ...options });
 
 export * from '@testing-library/react';
