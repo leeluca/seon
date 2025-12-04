4dd8c6adae3bf643a81ad0d93ab8926056f138d5
leeluca <30341309+leeluca@users.noreply.github.com>
2025-12-04 23:56:41 +0900

    refactor: add global providers to router

--- packages/web/src/routes/__root.tsx
+++ packages/web/src/routes/__root.tsx
@@ -1,17 +1,9 @@
-import { useEffect } from 'react';
-import { i18n } from '@lingui/core';
-import { I18nProvider } from '@lingui/react';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { createRootRouteWithContext, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 
-import { Toaster } from '~/components/ui/sonner';
-import { TooltipProvider } from '~/components/ui/tooltip';
-import { ViewportHandler } from '~/components/ViewportHandler';
-import { defaultLocale, dynamicallyImportLocale } from '~/locales/i18n';
-import OnlineStatusProvider from '~/states/isOnlineContext';
-import { useUserStore } from '~/states/stores/userStore';
-import SyncProvider from '~/states/syncContext';
+import ErrorFallback from '~/components/ErrorFallback';
+import NotFound from '~/components/NotFound';
 import type { AuthStatus } from '~/types/user';
 
 export interface RouterContext {
@@ -20,37 +12,20 @@ export interface RouterContext {
 }
 export const Route = createRootRouteWithContext<RouterContext>()({
   component: Root,
+  errorComponent: ({ error }) => {
+    return <ErrorFallback error={error} />;
+  },
+  notFoundComponent: () => {
+    return <NotFound className="min-h-dvh" />;
+  },
 });
 
 function Root() {
-  const languagePreference = useUserStore(
-    (state) => state.userPreferences?.language,
-  );
-
-  useEffect(() => {
-    const locale = languagePreference ?? defaultLocale;
-    void dynamicallyImportLocale(locale);
-    document.documentElement.lang = locale;
-  }, [languagePreference]);
-
   return (
-    <SyncProvider>
-      <I18nProvider i18n={i18n}>
-        <TooltipProvider delayDuration={300}>
-          <Toaster
-            position="top-right"
-            duration={2500}
-            closeButton
-            className="mt-6"
-          />
-          <OnlineStatusProvider>
-            <ViewportHandler />
-            <Outlet />
-          </OnlineStatusProvider>
-        </TooltipProvider>
-      </I18nProvider>
+    <>
+      <Outlet />
       <TanStackRouterDevtools position="bottom-right" />
       <ReactQueryDevtools position="bottom" buttonPosition="bottom-left" />
-    </SyncProvider>
+    </>
   );
 }
